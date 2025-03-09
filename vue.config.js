787b783d01477c7161bdfe6d7a9a972350e5bb77
nisino25 <nisino25@gmail.com>
2025-03-10 03:07:45 +0900

    minor updated

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
 
   pwa: {
-    name: "My Vue PWA",
+    name: "Running log",
     themeColor: "#1E1E1E",  // Match this with your actual background color
     msTileColor: "#1E1E1E", // Match for Windows Tiles
     appleMobileWebAppCapable: "yes",
