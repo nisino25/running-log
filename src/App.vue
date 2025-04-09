1b9c9501a43900ee4e5a49ed8301a014b7247d81
nisino25 <56564923+nisino25@users.noreply.github.com>
2025-04-09 09:45:02 +0900

    cange nthe url

--- src/App.vue
+++ src/App.vue
@@ -124,7 +124,7 @@ export default {
       loading: true,
       error: null,
       hasData: false,
-      baseUrl: "https://script.google.com/macros/s/AKfycbzsV8ncUnDdmEjLxIAIlmxfjnTK12ebSU598ESlSdS1LKTq1tOh58CFOquO52IJ7HYX/exec",
+      baseUrl: "https://script.google.com/macros/s/AKfycbxj_aD8kVSopROfqwMJbT0MhIt8-HhXu2j0mlpOKS_hYuFcda8Qx7sVeSMIWior1uL9/exec",
 
       swimmingLaps: null,
       weightNum: null,
